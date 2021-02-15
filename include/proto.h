d3ffe0a5172a94fb02cbfdeffc348fa13f976b48
qiu1969 <568385476@qq.com>
2021-02-15 16:13:46 +0800

    l

--- include/proto.h
+++ include/proto.h
@@ -1,7 +1,13 @@
 #ifndef QIUOS_PROTO_H_
 #define QIUOS_PROTO_H_
+#include "const.h"
 
+PUBLIC void setColor(u8 color);
+PUBLIC void putchar(int c);
+PUBLIC int puts(const char * str);
 
+PUBLIC u8 read_port(u16 port);
+PUBLIC void write_port(u16 port, u8 value);
 
 
 #endif // QIUOS_PROTO_H_
--- kernel/main.c
+++ kernel/main.c
@@ -1,16 +1,18 @@
 #include "type.h"
 #include "const.h"
+#include "proto.h"
 
 u32 dis_pos = 2400;             // 从第15行开始显示
 u8  dis_color = 0xf;            // 白色高亮
-
-void setColor(u8 color) { dis_color = color; }
-void putchar(int c);
-int puts(const char * str);
+u64 gdt[GDT_SIZE];              // gdt表
+u64 idt[IDT_SIZE];              // idt表
 
 int kernel_main()
 {
     setColor(HIGHLIGHT|FG_MAGENTA|BG_BLUE);
     puts("Welcome to QiuOS World!\nWelcome to QiuOS World!");
+    // 重新设置gdt表
+    // 设置idt表
+
     __asm__ volatile("hlt");
 }
